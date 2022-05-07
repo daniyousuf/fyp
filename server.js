4f8c2e2ffc902514162a8d1cbf5e5d250337abb2
Daniyal <d.yousuf98@gmail.com>
2022-05-07 06:14:28 +0500

    port setup

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.use('/api/feq', feq)
 app.use('/api/fiq', fiq)
 
 
-app.listen(7000, () => {
+app.listen(process.env.PORT || 7000, () => {
   console.log("Server is running at port 7000");
 });
